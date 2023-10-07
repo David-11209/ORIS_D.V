976c9e533d1164711b1783f84ccc353fe59c2d8d
David-11209 <limonchik087@gmail.com>
2023-10-07 12:39:02 +0300

    Homework_3_add_mail

--- Homework_3/HTTP_Server/HTTP_Server/Config.cs
+++ Homework_3/HTTP_Server/HTTP_Server/Config.cs
@@ -6,6 +6,19 @@ namespace HTTP_Server
         public string Address { get; set; }
         public int Port { get; set; }
         public string StaticDirectoryPath { get; set; }
+        public string Mail { get; set; }
+        public string MailPassword { get; set; }
+    }
+
+    class Mail
+    {
+        public Mail(string address, string password)
+        {
+            Address = address;
+            Password = password;
+        }
+        public string Address;
+        public string Password;
     }
 }
 
--- Homework_3/HTTP_Server/HTTP_Server/Server.cs
+++ Homework_3/HTTP_Server/HTTP_Server/Server.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Mail;
 using System.Text;
+using System.Web;
+using HTTP_Server;
 
 public class Server
 {
     private HttpListener listener;
     private string staticDir;
+    private static Mail mail;
+
+    public async Task<HttpListenerContext> GetContextAsync()
+    {
+        return await listener.GetContextAsync();
+    }
 
     public Server(string staticDir, string baseUrl)
     {
+        Config config = Settings.config;
         listener = new HttpListener();
         listener.Prefixes.Add(baseUrl);
+        mail = new Mail(config.Mail, config.MailPassword);
         this.staticDir = staticDir;
 
         if (!Directory.Exists(this.staticDir))
@@ -19,7 +30,36 @@ public class Server
             Directory.CreateDirectory(this.staticDir);
         }
     }
+    static void SendEmail(string message, string[] formData)
+    {
+        var address = HttpUtility.UrlDecode(formData[0].Split('=')[1]);
 
+        using (SmtpClient client = new SmtpClient("smtp.yandex.ru"))
+        {
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Port = 25;
+
+            client.Credentials = new NetworkCredential(mail.Address, mail.Password);
+    
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(mail.Address);
+            mailMessage.To.Add("dumbulgul@yandex.ru");
+            mailMessage.Subject = "Ты попался!";
+            mailMessage.Body = message;
+
+            try
+            {
+                client.Send(mailMessage);
+                Console.WriteLine("Mail Sent Successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to send mail: " + ex.Message);
+            }
+        }
+    }
     public void Start()
     {
         listener.Start();
@@ -27,17 +67,28 @@ public class Server
 
         while (true)
         {
-
             HttpListenerContext context = listener.GetContext();
             HttpListenerRequest request = context.Request;
             HttpListenerResponse response = context.Response;
-
+            var c = Settings.config;
             Console.WriteLine($"Запрос: {request.Url}");
-
             string filePath;
             if (request.Url.LocalPath.Equals("/"))
             {
-                filePath = filePath = Path.Combine(staticDir, "BattleNet", "index.html");
+                filePath = Path.Combine(staticDir, "BattleNet", "index.html");
+            }
+            else if (request.Url.LocalPath == "/sendEmail")
+            {
+                using (StreamReader reader = new StreamReader(request.InputStream))
+                {
+                    string data = reader.ReadToEnd();
+
+                    string[] formData = data.Split('&');
+
+                    var message = $"Ха-ха-хау!1!!";
+                    SendEmail(message , formData);
+                }
+                filePath = Path.Combine(staticDir, "BattleNet", "index.html");
             }
             else if (!request.Url.LocalPath.Contains('.'))
             {
@@ -49,7 +100,6 @@ public class Server
                 var b = a.Split('/');
                 filePath = b[b.Length - 4] + "/" + b[b.Length - 3] + "/" + b[b.Length - 2] + "/" + b[b.Length - 1];
             }
-
             if (File.Exists(filePath))
             {
                 var pageContents = File.ReadAllBytes(filePath);
@@ -66,9 +116,9 @@ public class Server
                 response.OutputStream.Write(Buffer, 0, Buffer.Length);
                 response.Close();
             }
+          
         }
     }
-
     private string GetContentType(string filePath)
     {
         string extension = Path.GetExtension(filePath).ToLower();
@@ -85,7 +135,6 @@ public class Server
         { ".gif", "image/gif" },
         { ".svg", "image/svg+xml" }
     };
-
         if (contentTypes.ContainsKey(extension))
         {
             return contentTypes[extension];
@@ -98,7 +147,6 @@ public class Server
                 return "application/unknown";
         }
     }
-
     public void Stop()
     {
         listener.Stop();
